c54cd8e1412a3bcf54eb3a9e3189d07a142aa41f
Nadine Seibel <nadine.seibel@gmail.com>
2016-05-12 11:56:24 +0200

    outsourced history stuff into extra file and a few mods to prompt

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -8,11 +8,6 @@ CASE_SENSITIVE="true"
 # Uncomment the following line to disable bi-weekly auto-update checks.
 DISABLE_AUTO_UPDATE="true"
 
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# The optional three formats: "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-HIST_STAMPS="dd.mm.yyyy"
-
 # User configuration
 
 export PATH=$HOME/bin:/usr/local/bin:$PATH
@@ -22,9 +17,5 @@ export PATH=$HOME/bin:/usr/local/bin:$PATH
 
 # Prefered editor
 export EDITOR='vim'
-
-# Bigger History
-HISTSIZE=20000
-SAVEHIST=20000
 
 ZLE_RPROMPT_INDENT=0
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -10,25 +10,33 @@ LIGHTNING="\u26a1"
 GEAR="\u2699"
 arrow_left=$'\ue0b0' # 
 arrow_sep=$'\ue0b1'
-arrow_right=$'\ue0b2' # 
-# hintergrundfarbe
-background="blue"
-# schriftfarbe
-foreground="black"    
-# user@rechner
-host=" %F{$foreground}%n@%m%f "
+arrow_right=$'\ue0b2' #    
 # directory
 dir=" %~ "
-# exit status
-#exit="%(?..%F{red}$cross%f)"
-exit=""
 # uhrzeit
-time="%F{$foreground}%T%f"
+time="%T"
 arrow_left_temp=""
 arrow_right_temp=""
+function ssh(){
+    if [ $SSH_CONNECTION ]; then
+      echo -n "SSH"
+    else
+      echo ""
+    fi
+}
+
+function usercolor(){
+    if [ "$(whoami)" = "root" ]; then
+      fontcolor="red"
+    else
+      fontcolor="green"
+    fi
+    echo -n "%F{$fontcolor}%n%f"      
+}
+
 function exit_status(){
     if [ "$?" = "1" ];
-      then echo -n "%F{red}$cross%f"
+      then echo -n " %F{red}$cross%f "
     else
       echo -n ""
     fi
@@ -43,6 +51,8 @@ fi
 }
 function build_prompt_left(){
     exit=$(exit_status)
+    user=$(usercolor)
+    host=" $user@%m "
     draw_segment_left 'green' '' $exit
     draw_segment_left 'yellow' 'red' $host
     draw_segment_left 'blue' 'red' $dir
@@ -51,11 +61,14 @@ function build_prompt_left(){
 }
 function draw_segment_right(){
     local bg=$1 fg=$2 content=$3
-    print -n "%K{$arrow_right_temp}%F{$bg}$arrow_right%f%k%K{$bg}%F{$fg}$content%f%k"
-    arrow_right_temp=$bg
+    print -n "%K{$arrow_right_temp}%F{$bg}$arrow_right%f%k%K{$bg}%F{$fg}$content%f%k"    
+     arrow_right_temp=$bg
 }
 function build_prompt_right(){
+    ssh=$(ssh)
     draw_segment_right 'green' 'red' $time
+    draw_segment_right 'blue'  'red' $ssh
+   # draw_segment_right 'green' 'red' $time
 }
 
 
