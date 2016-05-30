17255acc6889e6b4e5881adc3ac2f27595847c58
Nadine Seibel <nadine.seibel@gmail.com>
2016-05-30 13:44:35 +0200

    folder lenght now depends on terminal width

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -18,6 +18,7 @@ bg=236
 time="%F{$fg}%T%f"
 arrow_left_temp=""
 arrow_right_temp=""
+# output the ssh connection status (SSH if connected, else nothing)
 function check_ssh(){
     if [ $SSH_CONNECTION ]; then
         echo -n "%F{$fg}SSH%f"
@@ -25,8 +26,9 @@ function check_ssh(){
         echo ""
     fi
 }
+# colors the username dependend on who it is
 function usercolor(){
-    who=$(whoami)
+    local who=$(whoami)
     fontcolor="040"
     if [ "$who" = "root" ]; then
         fontcolor="088"
@@ -37,7 +39,7 @@ function usercolor(){
     fi
     echo -n "%F{$fontcolor}%n%f"
 }
-
+# display the exit status (red cross when 1 else empty)
 function exit_status(){
     if [ "$?" = "1" ]; then
         echo -n "%F{001}$cross%f"
@@ -45,21 +47,22 @@ function exit_status(){
         echo -n ""
     fi
 }
+# get the actual folder abridged to n segments
 function get_dir(){
-    max=$1
+    local max=$1
     akt_dir="${PWD/#$HOME/~}"
-    # trennzeichen zwischen ordnern
+    # seperator for folders
     seperator="%F{000}/%f"
-    # string der an gekürzten stellen angezeigt wird
+    # string showing where the path was abridged
     abridged="%F{$fg}..%f"
-    # anzahl von / im pfad
+    # number of / in path
     count_slash=$(grep -o "/" <<< "$akt_dir" | wc -l)
     array=()
-    # alle ordnernamen werden in array geschrieben
+    # put all folders in path in array
     for (( i=1; i<=$count_slash+1; i++ )); do
         array[$i]=$(cut -d/ -f$i <<< "$akt_dir")
     done
-    # true wenn der ganze pfad ausgegeben werden soll, sonst false
+    # true to return whole path, else false
     all=false
     if [ -z $max ] || [ $max = 0 ] || [ $max -ge $((count_slash+1)) ]; then
         let max=$count_slash+1
@@ -67,40 +70,45 @@ function get_dir(){
     fi
     elem=0
     newdir=""
-    # das aktuelle element im array ist das letzte
-    let akt_elem=$count_slash+1
+    # sets the pointer to the last element in array
+    let act_elem=$count_slash+1
     while [ $max -ge $((elem+1)) ]; do
-        # wenn schon was im string steht
+        # if output string not empty
         if [ ! -z $newdir ]; then
-            # wenn das nächste element das letzte sein soll (und nicht alle angezeigt werden sollen)
-            # echo "elem: $elem; elem+1: $((elem+1)); max: $max; all: $all"
+            # if next element will be the last (and not whole path is outputted)
             if [[ $((elem+1)) -eq $max && $all == false ]]; then
                 newdir="%F{$fg}${array[1]}%f$seperator$abridged$seperator$newdir"
-            # sonst
+            # else
             else
-                newdir="%F{$fg}${array[$akt_elem]}%f$seperator$newdir"
+                newdir="%F{$fg}${array[$act_elem]}%f$seperator$newdir"
             fi
-        # wenn der string leer ist aber nur ein element rein soll
-        elif [[ $max -eq 1 && $akt_elem -gt 1 ]]; then
-            newdir="$abridged%F{$fg}$array[$akt_elem]%f"
-        # wenn der string nur leer ist
+        # if output string is empty and will contain only one element
+        elif [[ $max -eq 1 && $act_elem -gt 1 ]]; then
+            newdir="$abridged%F{$fg}$array[$act_elem]%f"
+        # if output string is empty
         else
-            newdir="%F{$fg}$array[$akt_elem]%f"
+            newdir="%F{$fg}$array[$act_elem]%f"
         fi
         let elem=$elem+1
-        let akt_elem=$akt_elem-1
+        let act_elem=$act_elem-1
     done
     echo -n $newdir
 }
+# strips the string of all zsh format expressions
 get_visible_string() {
     local zero='%([BSUbfksu]|([FB]|){*})'
     echo -n ${${(S%%)1//$~zero}}
 }
+# outputs the current directory abridged to given lenght (30 default)
 function directory(){
+    local len=$1
+    if [ -z $len ] || [ $len -eq 0 ]; then
+        len=30
+    fi
     akt_dir="$(get_dir 0)"
     visible_dir="$(get_visible_string $akt_dir)"
     count=10
-    while [ ${#visible_dir}  -gt 30 ]; do
+    while [ ${#visible_dir} -gt $len ]; do
         akt_dir="$(get_dir $count)"
         visible_dir="$(get_visible_string $akt_dir)"
         let count=$count-1
@@ -110,6 +118,19 @@ function directory(){
     done
     echo -n $akt_dir
 }
+# returns the lenght of the directory path dependend on the terminal width
+function dir_len(){
+    if [ $COLUMNS -le 80 ]; then
+        echo 20
+    elif [ $COLUMNS -le 160 ]; then
+        echo 30
+    elif [ $COLUMNS -le 180 ]; then
+        echo 40
+    else
+        echo 50
+    fi
+}
+# draw a prompt segment for the left side of the prompt
 function draw_segment_left(){
     local bg=$1 content=$2
     if [ ! -z $content ]; then
@@ -117,6 +138,7 @@ function draw_segment_left(){
         arrow_left_temp="%F{$bg}$arrow_left%f"
     fi
 }
+# build the left side prompt
 function build_prompt_left(){
     exitvar=$(exit_status)
     user=$(usercolor)
@@ -124,12 +146,13 @@ function build_prompt_left(){
     let color_1=$bg
     let color_2=$bg+2
     host="$user%F{000}@%f%F{$fg}%m%f"
-    akt_dir=$(directory)
+    akt_dir=$(directory $(dir_len))
     draw_segment_left $color_0 $exitvar
     draw_segment_left $color_1 $host
     draw_segment_left $color_2 $akt_dir
     print -n $arrow_left_temp
 }
+# draw a prompt segment for the right side of the prompt
 function draw_segment_right(){
     local bg=$1 content=$2
     if [ ! -z $content ]; then
@@ -137,6 +160,7 @@ function draw_segment_right(){
         arrow_right_temp=$bg
     fi
 }
+# build the right side prompt
 function build_prompt_right(){
     let color_0=$bg
     let color_1=$bg-2
