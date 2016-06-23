c146485275293b2537c0aed247dac915b8b1f9ed
Daenara <nadine@daenara.net>
2016-06-23 12:17:59 +0200

    added i3lock-color config2

---
+++ i3lock/i3lock-sao.sh
@@ -0,0 +1,13 @@
+incl=fbf5f560
+invcl=505050aa
+inwcl=$invcl
+ricl=ffffffee
+rivcl=ffffffff
+riwcl=$rivcl
+khlcl=505050aa
+bhlcl=505050aa
+tecl=ffffffff
+licl=505050aa
+img=/usr/share/sddm/themes/sao-grasslands/background.png
+
+i3lock -i $img --insidecolor=$incl --insidevercolor=$invcl --insidewrongcolor=$inwcl --ringcolor=$ricl --ringvercolor=$rivcl --ringwrongcolor=$riwcl --keyhlcolor=$khlcl --bshlcolor=$bhlcl --textcolor=$tecl --linecolor=$licl
