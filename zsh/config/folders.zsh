27007868fc2cd6a07e3b5ee0d826b1f55b7f3ba7
Daenara <nadine@daenara.net>
2017-01-10 21:14:46 +0100

    added command for saving work directory seperate from the currend directory

--- zsh/config/folders.zsh
+++ zsh/config/folders.zsh
@@ -3,9 +3,13 @@ function cd() {
     builtin cd "$@";
     echo "$PWD" > $ZSH/run/currentfolder;
 }
+function swd() {
+    echo "$PWD" > $ZSH/run/workingdirectory;
+}
 export cd
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
-alias cwd='cd "$(cat $ZSH/run/currentfolder)"'
+alias cds='cd "$(cat $ZSH/run/currentfolder)"'
+alias cwd='cd "$(cat $ZSH/run/workingdirectory)"'
 
